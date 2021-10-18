688d55265567de87dcf30b68125cf2e7cfb16bbd
Dewdunu Perera <72658565+dewdunu@users.noreply.github.com>
2021-10-18 15:37:47 +0530

    Create users.controllers.js

---
+++ server/app/controllers/users.controllers.js
@@ -0,0 +1,13 @@
+const User = require("../models/users.model.js");
+
+// Retrieve all Customers from the database.
+exports.findAll = (req, res) => {
+    User.getAll((err, data) => {
+      if (err)
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving users."
+        });
+      else res.send(data);
+    });
+  };
